f9a5aa94d532ada1a4383fd3c56f5d9a5efbf93d
Paul Hiles <paul@hiles.ca>
2020-04-28 16:56:09 -0400

    Change index to php and add Foundation6 assets

---
+++ sites/default/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Dockered PHP/Apache :: Default Site</title>
+		<link rel="stylesheet" href="assets/css/app.css">
+	</head>
+	<body>
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x">
+				<div class="large-12 cell">
+					<h1>Dockered PHP/Apache</h1>
+					<h3>Default VHost</h3>
+				</div>
+			</div>
+			<div class="grid-x grid-padding-x">
+				<div class="large-12 cell">
+					<h4>Environment</h4>
+					<ul>
+						<li>
+							Apache
+						</li>
+						<li>
+							PHP
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="grid-x grid-padding-x">
+				<div class="large-12 cell">
+					<h4>Test Links</h4>
+					<ul>
+						<li>
+							<a href="info.php">PHP Info</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	<script src="assets/js/app.js"></script>
+	</body>
+	</html>
